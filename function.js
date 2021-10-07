0f19b4f891e7ec3d1e5042c28443ec67f5f2902c
shavkodima <shavkodima10@gmail.com>
2021-10-07 11:37:07 +0300

    update function finish

--- function.js
+++ function.js
@@ -52,5 +52,49 @@ function printFarmInventory(cows, chickens, pigs) {
      console.log(zeroPad(chickens, 3) + "	Куриц");
       console.log(zeroPad(pigs, 3) + "	Свиней"); 
     }
-    
+
 printFarmInventory(7, 16, 3);
+
+//Упражнение
+// 1.Напишите функцию	min,	принимающую	два	аргумента,	и возвращающую	минимальный	из	них
+
+function min(a,b){
+    let res = Math.min(a, b)
+    return res;
+}
+
+console.log(min(5,6));
+
+
+
+//Рекурсия
+// Напишите	рекурсивную	функцию	isEven	согласно	этим правилам.	Она	должна	принимать	число	и	возвращать булевское	значение.
+//выполнена не до конца
+
+function isEven(number){
+    if(number%2 == 0){
+        return true;
+    }else{
+        return false;
+    }
+}
+
+console.log(isEven(76));
+
+
+//Напишите	функцию	countBs,	которая	принимает	строку	в качестве	аргумента,	и	возвращает	количество	символов “B”,	содержащихся	в	строке.
+
+function countBs(string, simvol){
+    let counter =0;
+    for(let i =0; i<string.length; i++){
+        //ищет символ в строке по индексу
+        if(string.charAt(i).toLowerCase() == simvol){
+           counter++;
+        }
+    }
+
+    console.log(counter);
+}
+
+
+countBs("Тестовая строка, ответ не требуется", "о")
